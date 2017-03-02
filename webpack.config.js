1cc071a2d8126b8a91bb0e9d426e98d580f4c4a8
greedying <greedying@163.com>
2017-03-02 11:18:44 +0800

    优化，增加对css、图片的处理，更改eslint规则

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
 const env  = require('yargs').argv.env; // use --env with webpack 2
+const version = require('./package.json').version
 
 let libraryName = 'library';
 
@@ -11,10 +12,14 @@ let plugins = [], outputFile;
 
 if (env === 'build') {
   plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
+  outputFile = `${libraryName}-${version}.min.js`;
 } else {
-  outputFile = libraryName + '.js';
+  outputFile = `${libraryName}-${version}.js`;
 }
+
+plugins.push(new webpack.LoaderOptionsPlugin({
+  minimize: true
+}))
 
 const config = {
   entry: __dirname + '/src/index.js',
@@ -37,11 +42,23 @@ const config = {
         test: /(\.jsx|\.js)$/,
         loader: "eslint-loader",
         exclude: /node_modules/
+      },
+      { 
+        test: /\.css$/, 
+        loader: "style-loader!css-loader"
+      },
+      { 
+        test: /\.less$/, 
+        loader: "style-loader!css-loader!less-loader"
+      },
+      {
+      　test: /\.(png|jpg|gif)$/,
+      　loader: 'url-loader?limit=8192'
       }
     ]
   },
   resolve: {
-    modules: [path.resolve('./src')],
+    modules: [path.resolve('./src'), path.resolve('./node_modules')],
     extensions: ['.json', '.js']
   },
   plugins: plugins
